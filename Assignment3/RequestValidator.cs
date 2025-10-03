cd520dd2df5ccac592525d72e003f05a5d4c261b
Frederik Bressendorff <stud-bressendor@ruc.dk>
2025-10-03 09:53:05 +0200

    Date Validation Request Validator

--- Assignment3/RequestValidator.cs
+++ Assignment3/RequestValidator.cs
@@ -14,11 +14,11 @@ namespace Assignment3
             var list = new List<string>();
             ValidateMethod(request, ref list);
             ValidatePath(request, ref list);
+            ValidateDate(request, ref list);
 
-
-
-
-            return new Response() { Status = string.Join(", ", list) };
+            var response = new Response();
+            response.Status = list.Count > 0 ? "4 " + string.Join(", ", list) : string.Empty; // Append Bad Request status code if there are errors
+            return response;
         }
 
         #region Method Validation
@@ -30,10 +30,11 @@ namespace Assignment3
 
             var validMethods = new List<string>()
             {
-                "get",
-                "post",
+                "create",
+                "read",
                 "update",
                 "delete",
+                "echo"
             };
             var methodInvalid = !validMethods.Contains(request.Method);
             var methodInvalidMessage = "illegal method";
@@ -47,6 +48,20 @@ namespace Assignment3
         private void ValidatePath(Request request, ref List<string> errorList)
         {
             if (string.IsNullOrWhiteSpace(request.Path)) errorList.Add("missing path");
+        }
+        #endregion
+
+        #region Date Validation
+        private void ValidateDate(Request request, ref List<string> errorList)
+        {
+            if (string.IsNullOrWhiteSpace(request.Date))
+            {
+                errorList.Add("missing date");
+                return;
+            }
+
+            var parseSuccess = long.TryParse(request.Date, out long _);
+            if (!parseSuccess) errorList.Add("illegal date");
         }
         #endregion
 
