73f83626cee4cde8777b4b29fd2f9a5d2c8e134b
dipika.bajracharya <dipika.bajracharya@nextly.solutions>
2025-10-05 15:11:21 +0200

    RequestValidator_NoPath_ShouldReturnMissingPath and RequestValidator_InvalidMethod_ShouldReturnIllegalMethod

--- Assignment3/RequestValidator.cs
+++ Assignment3/RequestValidator.cs
@@ -10,87 +10,23 @@ namespace Assignment3
 {
     public class RequestValidator
     {
+
         public Response ValidateRequest(Request request)
         {
-            var list = new List<string>();
-            ValidateMethod(request, ref list);
-            ValidatePath(request, ref list);
-            ValidateDate(request, ref list);
-            ValidateBody(request, ref list);
-
-            var response = new Response();
-            response.Status = list.Count > 0 ? "4 " + string.Join(", ", list) : "1 Ok"; // Append Bad Request status code if there are errors
-            return response;
-        }
-
-        #region Method Validation
-        private void ValidateMethod(Request request, ref List<string> errorList)
-        {
-            var methodMissing = string.IsNullOrWhiteSpace(request.Method);
-            var methodMissingMessage = "missing method";
-            if (methodMissing) errorList.Add(methodMissingMessage);
-
-            var validMethods = new List<string>()
+            if (string.IsNullOrEmpty(request.Method?.Trim()))
             {
-                "create",
-                "read",
-                "update",
-                "delete",
-                "echo"
-            };
-            var methodInvalid = !validMethods.Contains(request.Method);
-            var methodInvalidMessage = "illegal method";
-            if (methodInvalid) errorList.Add(methodInvalidMessage);
-        }
+                return new Response { Status = "missing method" };
 
-
-        #endregion
-
-        #region Path Validation
-        private void ValidatePath(Request request, ref List<string> errorList)
-        {
-            if (string.IsNullOrWhiteSpace(request.Path)) { errorList.Add("missing path"); return; }
-            if (!(new UrlParser()).ParseUrl(request.Path)) errorList.Add("illegal path");
-        }
-        #endregion
-
-        #region Date Validation
-        private void ValidateDate(Request request, ref List<string> errorList)
-        {
-            if (string.IsNullOrWhiteSpace(request.Date))
-            {
-                errorList.Add("missing date");
-                return;
             }
+            string[] validMethods = { "create", "read", "echo", "delete", "echo" };
 
-            var parseSuccess = long.TryParse(request.Date, out long _);
-            if (!parseSuccess) errorList.Add("illegal date");
-        }
-        #endregion
-
-        #region Body Validation
-        private void ValidateBody(Request request, ref List<string> errorList)
-        {
-            var requiresBody = new List<string>() { "update", "create", "echo" };
-            if (!requiresBody.Contains(request.Method)) return;
-
-            if (string.IsNullOrWhiteSpace(request.Body))
+            if (!validMethods.Contains(request.Method.ToLower()))
             {
-                errorList.Add("missing body");
-                return;
+                return new Response { Status = "illegal method" };
             }
-
-            try
-            {
-                var parse = JsonSerializer.Deserialize<object>(request.Body);
-            }
-            catch (Exception ex)
-            {
-                errorList.Add("illegal body");
-            }
+            return new Response { };
         }
 
 
-        #endregion
     }
 }
