db42814283a660002e8098cef1bbf89f3e407a6a
Neva Shrestha <shresthaneva11@gmail.com>
2025-10-07 23:14:55 +0545

    CategoryService

--- Assignment3/CategoryService.cs
+++ Assignment3/CategoryService.cs
@@ -2,36 +2,68 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Assignment3
 {
     public class CategoryService : ICategoryService
     {
-        public bool CreateCategory(int id, string name)
+        // creating an in-memory database
+        private readonly List<Category> _categories;
+
+        public CategoryService()
         {
-            throw new NotImplementedException();
+            // default data initialized
+            _categories = new List<Category>
+            {
+                new Category { cid = 1, name = "Beverages" },
+                new Category { cid = 2, name = "Condiments" },
+                new Category { cid = 3, name = "Confections" }
+            };
         }
 
-        public bool DeleteCategory(int id)
-        {
-            throw new NotImplementedException();
-        }
-
+        // show all categories
         public List<Category> GetCategories()
         {
-            throw new NotImplementedException();
+            return _categories;
         }
 
-        public Category GetCategory(int cid)
+        // show a specific category by ID 
+        public Category? GetCategory(int cid)
         {
-            throw new NotImplementedException();
+            return _categories.FirstOrDefault(c => c.cid == cid);
         }
 
+        // create a new category with auto-geterating IDs 
+        private int _nextId = 4;
+
+        public Category CreateCategory(string name)
+        {
+            var cat = new Category { cid = _nextId++, name = name };
+            _categories.Add(cat);
+            return cat;
+        }
+
+
+        // update an existing category
         public bool UpdateCategory(int id, string newName)
         {
-            throw new NotImplementedException();
+            var category = _categories.FirstOrDefault(c => c.cid == id);
+            if (category == null)
+                return false;
+
+            category.name = newName;
+            return true;
+        }
+
+        // delete an existing category 
+        public bool DeleteCategory(int id)
+        {
+            var category = _categories.FirstOrDefault(c => c.cid == id);
+            if (category == null)
+                return false;
+
+            _categories.Remove(category);
+            return true;
         }
     }
 }
